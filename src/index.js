8aeae7b6d2aa31f04ad501127049036317b919d7
renercrows <ungredda95@gmail.com>
2022-08-03 11:55:45 -0400

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import './module/consumeAPI.js';
-import logo from "./assets/images/restaurant.jpg";
+import logo from './assets/images/restaurant.jpg';
 
-const PageImage = document.querySelector(".page-img");
+const PageImage = document.querySelector('.page-img');
 PageImage.src = logo;
--- src/module/consumeAPI.js
+++ src/module/consumeAPI.js
@@ -23,13 +23,12 @@ const fetchMeal = async () => {
     });
   };
 
-  const fetchURL =
-    "https://www.themealdb.com/api/json/v1/1/filter.php?a=american";
+  const fetchURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=american';
   const meals = await fetch(fetchURL)
     .then((response) => response.json())
     .then((data) => data.meals);
   PopulateMeals(meals);
-  const MealCount = document.querySelector(".menuCount");
+  const MealCount = document.querySelector('.menuCount');
   MealCount.innerHTML = `${meals.length}`;
 };
 
