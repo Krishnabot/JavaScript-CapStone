9363543061bab71246322a4d902c0d4f8a3a56bd
Krishna <libraryismyparadise@gmail.com>
2022-08-05 02:19:53 +0545

    Add GET ADD LIKE Function

--- src/module/consumeAPI.js
+++ src/module/consumeAPI.js
@@ -1,9 +1,11 @@
-const MealList = document.querySelector('.meal-list');
+import { Displaylikes, like } from "./GetAddLike.js";
+
+const MealList = document.querySelector(".meal-list");
 const fetchMeal = async () => {
   const PopulateMeals = (meals) => {
     meals.forEach((element) => {
-      const newMeal = document.createElement('div');
-      newMeal.classList.add('meal-container');
+      const newMeal = document.createElement("div");
+      newMeal.classList.add("meal-container");
       newMeal.innerHTML = ` 
       <div class="meal-card">
         <div class ="meal-img">
@@ -12,8 +14,8 @@ const fetchMeal = async () => {
         <div class="meal-info">
           <h4 class="meal-name">${element.strMeal}</h4>
           <div class = "meal-reaction">
-          <button class="like" data="${element.idMeal}"> Like </button>
-          <button class ="comment" data="${element.idMeal}">Comment</button>
+<button class = "like"><span class= "likecount" data="${element.idMeal}">0</span><i class="fas fa-heart" data="${element.idMeal}"></i></button>         
+<button class ="comment" data="${element.idMeal}">Comment</button>
           </div>
           </div>
           </div>
@@ -23,13 +25,24 @@ const fetchMeal = async () => {
     });
   };
 
-  const fetchURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=american';
+  const fetchURL =
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=american";
   const meals = await fetch(fetchURL)
     .then((response) => response.json())
     .then((data) => data.meals);
   PopulateMeals(meals);
-  const MealCount = document.querySelector('.menuCount');
+  const MealCount = document.querySelector(".menuCount");
   MealCount.innerHTML = `${meals.length}`;
+
+  const LikeCount = document.querySelectorAll(".likecount");
+  const btnsArr = Array.from(LikeCount);
+  for (let i = 0; i < btnsArr.length; i += 1) {
+    btnsArr[i].addEventListener("click", () => {
+      setTimeout(() => like(), 2000);
+      Displaylikes();
+    });
+  }
 };
 
 fetchMeal();
+Displaylikes();
