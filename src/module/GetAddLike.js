149ac56ebc9443a2fbffbe499ef691924d461673
Krishna <libraryismyparadise@gmail.com>
2022-08-06 01:46:23 +0545

    Fix Linter Errors

--- src/module/GetAddLike.js
+++ src/module/GetAddLike.js
@@ -1,15 +1,15 @@
 const Displaylikes = async () => {
   const likeditems = await fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fqnn9lAjGXIKn1TiiiZP/likes"
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fqnn9lAjGXIKn1TiiiZP/likes',
   )
     .then((response) => response.json())
     .then((data) => data);
 
-  const LikeCounter = document.getElementsByClassName("likecounter");
+  const LikeCounter = document.getElementsByClassName('likecounter');
   const likeCountArray = Array.from(LikeCounter);
 
   likeCountArray.forEach((likecounter) => {
-    const likeId = likecounter.getAttribute("data");
+    const likeId = likecounter.getAttribute('data');
     const element = likecounter;
     likeditems.forEach((e) => {
       if (likeId === e.item_id) {
@@ -21,27 +21,27 @@ const Displaylikes = async () => {
 
 const PostLike = async (itemid) => {
   await fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fqnn9lAjGXIKn1TiiiZP/likes",
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fqnn9lAjGXIKn1TiiiZP/likes',
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         item_id: itemid,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    }
+    },
   ).then(() => {
     Displaylikes();
   });
 };
 const like = () => {
-  const likebtns = document.getElementsByClassName("fa-heart");
+  const likebtns = document.getElementsByClassName('fa-heart');
   const likeCountArray = Array.from(likebtns);
   likeCountArray.forEach((element) => {
-    const likeId = element.getAttribute("data");
-    element.addEventListener("click", () => {
-      element.classList.add("heart-active");
+    const likeId = element.getAttribute('data');
+    element.addEventListener('click', () => {
+      element.classList.add('heart-active');
       PostLike(likeId);
       Displaylikes();
     });
--- src/module/consumeAPI.js
+++ src/module/consumeAPI.js
@@ -1,11 +1,11 @@
-import { Displaylikes, like } from "./GetAddLike.js";
+import { Displaylikes, like } from './GetAddLike.js';
 
-const MealList = document.querySelector(".meal-list");
+const MealList = document.querySelector('.meal-list');
 const fetchMeal = async () => {
   const PopulateMeals = (meals) => {
     meals.forEach((element) => {
-      const newMeal = document.createElement("div");
-      newMeal.classList.add("meal-container");
+      const newMeal = document.createElement('div');
+      newMeal.classList.add('meal-container');
       newMeal.innerHTML = ` 
       <div class="meal-card">
         <div class ="meal-img">
@@ -25,19 +25,18 @@ const fetchMeal = async () => {
     });
   };
 
-  const fetchURL =
-    "https://www.themealdb.com/api/json/v1/1/filter.php?a=american";
+  const fetchURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=american';
   const meals = await fetch(fetchURL)
     .then((response) => response.json())
     .then((data) => data.meals);
   PopulateMeals(meals);
-  const MealCount = document.querySelector(".menuCount");
+  const MealCount = document.querySelector('.menuCount');
   MealCount.innerHTML = `${meals.length}`;
 
-  const LikeCount = document.querySelectorAll(".likecount");
+  const LikeCount = document.querySelectorAll('.likecount');
   const btnsArr = Array.from(LikeCount);
   for (let i = 0; i < btnsArr.length; i += 1) {
-    btnsArr[i].addEventListener("click", () => {
+    btnsArr[i].addEventListener('click', () => {
       setTimeout(() => like(), 2000);
       Displaylikes();
     });
