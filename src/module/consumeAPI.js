6710131477f124d5d05db9be74de7e45c6a5b699
Krishna <libraryismyparadise@gmail.com>
2022-08-06 02:36:33 +0545

    Fix Linter Errors

--- src/module/consumeAPI.js
+++ src/module/consumeAPI.js
@@ -1,12 +1,12 @@
 import { Displaylikes, like } from './GetAddLike.js';
-import DisplayCount from "./count.js";
+import DisplayCount from './count.js';
 
 const MealList = document.querySelector('.meal-list');
 const fetchMeal = async () => {
   const PopulateMeals = (meals) => {
     meals.forEach((element) => {
-      const newMeal = document.createElement("div");
-      newMeal.classList.add("meal-container");
+      const newMeal = document.createElement('div');
+      newMeal.classList.add('meal-container');
       newMeal.innerHTML = ` 
       <div class="meal-card">
         <div class ="meal-img">
@@ -26,17 +26,16 @@ const fetchMeal = async () => {
     });
   };
 
-  const fetchURL =
-    "https://www.themealdb.com/api/json/v1/1/filter.php?a=american";
+  const fetchURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=american';
   const meals = await fetch(fetchURL)
     .then((response) => response.json())
     .then((data) => data.meals);
   PopulateMeals(meals);
 
-  const LikeCount = document.querySelectorAll(".likecount");
+  const LikeCount = document.querySelectorAll('.likecount');
   const btnsArr = Array.from(LikeCount);
   for (let i = 0; i < btnsArr.length; i += 1) {
-    btnsArr[i].addEventListener("click", () => {
+    btnsArr[i].addEventListener('click', () => {
       setTimeout(() => like(), 2000);
       Displaylikes();
     });
