c374bbf6d1d95b4b2c9ecf6a39f5e9214d691419
Krishna <libraryismyparadise@gmail.com>
2022-08-11 12:25:59 +0545

    Style PopUp

--- src/module/popup.js
+++ src/module/popup.js
@@ -20,35 +20,34 @@ const popup = async () => {
         <div class="card">
         <div class="meal-header">
             <i class="fas fa-times close-pop-up"></i>
-            <h2 class="titme">${element.strMeal}</h2>
+            <h2 class ="meal-name">${element.strMeal}</h2>
         </div>
         <div class="popup-img">
             <img class="popup-img" src="${element.strMealThumb}" alt="${element.strMeal}">
         </div>
+        <div class="cuisine">
+        <h2> Cuisine: ${element.strArea}<h2>
+          </div>
         <div class="popup-columns">
         <div class = "left-columns">
-        <p> <span class="description-header">Area:</span> ${element.strArea} <p>
-        <p> <span class="description-header">Ingredients:</span>
+        <h3 class="ingridient">Ingredients:</h3>
         <ul id = "ingredientsUl">
         </ul>
         </span><p>
         </div>
         <div class = "right-columns">
-        <p> <span class="description-header">Catagory:</span> ${element.strCatagory} <p>
-        <p> <span class="description-header">Instructions:</span> <p> <span class = "recipe-instrruction"> ${element.strInstructions}  > read more </button> </span>
+        <h3 class="description-header">Recipe : </h3>
+        <p class = "recipe-instrruction"> ${element.strInstructions}</p>
         </div>
         </div>
-        <div class="comments-container">
-          <div class="all-comments text-center">
+    <h1 class="leave-comment"> Looks Delicious? Leave A Comment </h1>
+        <div class="comments-wrap">
+          <div class="display-comments">
             <h3>Comments <span id= "comments-count">0</span> </h3>
             <ul id="comments-ul">
-
             </ul>
           </div>
-          <div class = "text-center2">
-            <h3> Add a comment </h3> 
-          </div>
-      
+
      <div class="add-comments">
       <form action="POST">
           <input name= "name" type="text" id="input-name" placeholder="Your name" required>
@@ -58,6 +57,7 @@ const popup = async () => {
           
       </form>
     </div>
+    </div> 
     </div>
     </div>
       `;
