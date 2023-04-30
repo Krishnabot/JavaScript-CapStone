6231d0d97be293defc7dee87fd98b4794ef94b51
Krishna <libraryismyparadise@gmail.com>
2023-04-30 14:40:34 +0545

    Fix Meal Count

--- src/module/count.js
+++ src/module/count.js
@@ -1,15 +1,16 @@
-const mealCount = document.getElementsByClassName('menuCount');
-const DisplayCount = async (area) => {
-  const fetchURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=american';
-  const meals = await fetch(fetchURL)
-    .then((res) => res.json())
-    .then((data) => data.meals);
+const DisplayCount = async () => {
+  const mealCount = document.getElementsByClassName('menuCount');
+  const areas = ['american', 'french', 'chinese'];
 
   for (let i = 0; i < mealCount.length; i += 1) {
-    mealCount[i].innerHTML = '';
-    if (mealCount[i].classList.contains(area)) {
-      mealCount[i].innerHTML = `(${meals.length})`;
-    }
+    const area = areas[i];
+    const fetchURL = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`;
+    const meals = await fetch(fetchURL)
+      .then((res) => res.json())
+      .then((data) => data.meals);
+
+    mealCount[i].innerHTML = `(${meals.length})`;
   }
 };
+
 export default DisplayCount;
